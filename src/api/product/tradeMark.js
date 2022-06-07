e17b64bf2734ea8c518d2bf321c0616d608cdafd
rxq_home <rxq@home.com>
2022-06-08 01:12:22 +0800

    P130

--- src/api/product/tradeMark.js
+++ src/api/product/tradeMark.js
@@ -7,3 +7,21 @@ export const reqTradeMarkList=(page,limit)=>{
     methods:'get'
   })
 }
+//  新增/修改 品牌
+export const reqAddOrUpdateTradeMark=(tradeMark)=>{
+  //  带给服务器请求包含id 则为修改请求
+  if(tradeMark.id){
+    return request({
+      url:`/admin/product/baseTrademark/update`,
+      method:'put',
+      data:tradeMark
+    })
+  }else{
+    return request({
+      url:`/admin/product/baseTrademark/save`,
+      method:'post',
+      data:tradeMark
+    })
+  }
+
+}
